2707377fb5908f036c3287e403c6aeb73bad7aed
Mihail Boboshko <vr.boboshko@gmail.com>
2019-05-13 14:03:32 +0500

    123

---
+++ ruby01/ruby001.rb
@@ -0,0 +1,32 @@
+age = 18
+puts(age)
+
+age = 24
+puts(age)
+
+text = "Привет всем, я Георгий"
+puts(text)
+
+happiness = 0.98
+puts(happiness)
+person_has_car = false
+puts(person_has_car)
+none = nil
+print("-",none,"-")
+words = "Вебсайт ИТ прогрер"
+word = "!"
+puts (words.upcase() + word)
+puts (words.downcase() + word)
+puts (words.length())
+puts (words.include? "Веб-")
+res = 2**3
+puts (res)
+puts (words +"-"+ res.to_s)
+num = -14.65
+puts (num.abs())
+puts (num.round())
+puts Math.sqrt(144)
+
+#puts("Введите имя пользователя")
+#name = gets.champ()
+#puts("Введите свое имя"+ name +", ты крут")
